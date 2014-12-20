bb8f8572df75418817ece7cdb5498ed6f2034533
Andre Garrigo <andre+git@garrigo.net>
2014-12-19 23:48:04 -0500

    Add placeholder text to email signup form

--- web/app/themes/launcheffect/launch/launch-form.php
+++ web/app/themes/launcheffect/launch/launch-form.php
@@ -32,29 +32,29 @@
 												<div class="reuserbubble-arrow"></div>
 											</div>
 										</a>
-										<?php endif; ?>	
+										<?php endif; ?>
 
 									</label>
 									<?php endif; ?>
-									
+
 									<!-- START IF NO CUSTOM FIELDS //////////////////////////////////////////////////////////////////// -->
 									<?php if(!get_option('lefx_cust_field1')) : ?>
-										
+
 								    <div id="email-layout">
 								        <span id="emailinput">
-								            <input type="email" id="email" name="email" />
+								            <input type="email" id="email" name="email" placeholder="Enter your email" />
 								        </span>
 										<!-- SUBMIT BUTTON -->
 										<span id="submit-button-border">
 											<span id="submit-button-spinner"></span>
 											<input type="submit" name="submit" value="<?php if(ler('label_submitbutton')) { le('label_submitbutton'); } else { echo 'GO'; } ?>" id="submit-button" />
-										</span>		
+										</span>
 								    </div>
-									
+
 									<!-- ERROR MESSAGING -->
 									<div id="error"></div>
 								</li>
-								
+
  								<?php if(get_option('lefx_captcha')) : ?>
 
 								<!-- START CAPTCHA -->
@@ -71,14 +71,14 @@
 								<?php else: ?>
 
 									<input type="email" id="email" name="email" />
-									
+
 									<!-- ERROR MESSAGING -->
 									<div id="error"></div>
 								</li>
 								<?php endif; ?>
-								
+
 								<!-- INCLUDE CUSTOM FIELDS (Premium) -->
-								<?php if(lefx_version() == 'premium') get_template_part('premium/custom','fields'); ?>				
+								<?php if(lefx_version() == 'premium') get_template_part('premium/custom','fields'); ?>
                                 <?php if(get_option('lefx_cust_field1')) : ?>
 
                                 <?php if(get_option('lefx_captcha')) : ?>
@@ -94,7 +94,7 @@
                                 </li>
 								<?php endif; ?>
 
-								<li class="hascf-submit-button">					
+								<li class="hascf-submit-button">
 									<!-- SUBMIT BUTTON -->
 									<span id="submit-button-border">
 										<span id="submit-button-spinner"></span>
@@ -108,56 +108,56 @@
 								<li>
 									<span class="privacy-policy">
 										<?php le('lefx_privacy_policy_label');?> <a href="#" id="modal-privacy" class="modal-trigger"><?php le('lefx_privacy_policy_heading'); ?></a>
-									</span>	
+									</span>
 								</li>
-								<?php endif; ?>	
+								<?php endif; ?>
 
 							</ul>
 						</fieldset>
 					</form>
-					
+
 					<!-- FORM (POST-SIGNUP) -->
 					<form id="success" action="">
-						<fieldset>		
+						<fieldset>
 
 							<!-- RETURNING USER -->
 							<div id="returninguser">
 								<h2><?php le('returning_subheading'); ?></h2>
-								
+
 								<?php if(is_page_template( 'launch.php' ) && ($lefx_content_placement = ler('lefx_editorcontent_placement')) && 'After Subheading' == $lefx_content_placement) : ?>
 								<!-- LOOP CONTENT (AFTER SUBHEADING) -->
 								<div id="signup-editor-content" class="clearfix"><?php the_content(); ?></div>
 								<?php endif; ?>
-							
+
 								<p>
 									<?php le('returning_text'); ?> <span class="user"> </span>.<br />
-						
+
 									<span <?php if(get_option('disable_unique_link') == 'true') echo ' class="disable"'; ?>>
 										<span class="clicks"> </span>&nbsp;<?php le('returning_clicks'); ?><br />
 									</span>
-								
+
 									<span <?php if(get_option('disable_unique_link') == 'true') echo ' class="disable"'; ?>>
 										<span class="conversions"></span>&nbsp;<?php le('returning_signups'); ?>
 
 									</span>
 									<br /><br />
-								</p>	
-									
+								</p>
+
 								<?php if(is_page_template( 'launch.php' ) && 'After Body Text' == $lefx_content_placement) : ?>
 								<!-- LOOP CONTENT (AFTER SUBHEADING) -->
 								<div id="signup-editor-content"><?php the_content(); ?></div>
 								<?php endif; ?>
-											
+
 							</div>
-							
+
 							<?php if(get_option('disable_social_media') != 'true') : ?>
 
 							<!-- SOCIAL MEDIA BUTTONS -->
 							<div class="signup-left">
 								<?php get_template_part('launch/launch','social'); ?>
-							</div>	
+							</div>
 							<?php endif; ?>
-							
+
 							<!-- RETURNING USER REFERRAL URL -->
 							<div id="returninguserurl">
 								<div class="signup-right<?php if(get_option('disable_unique_link') == 'true') echo ' disable'; ?>">
@@ -168,8 +168,8 @@
 
 									<input type="text" id="returningcode" value="" onclick="LE_Handlers.SelectAll('returningcode');"/>
 								</div>
-							</div>				
-							
+							</div>
+
 							<!-- NEW USER -->
 							<div id="newuser">
 								<div class="signup-right<?php if(get_option('disable_unique_link') == 'true') { echo ' disable'; } ?>">
@@ -178,10 +178,10 @@
 									<label for="email"><?php le('label_success_content'); ?></label>
 									<?php endif; ?>
 
-									<input type="text" id="successcode" value="" onclick="LE_Handlers.SelectAll('successcode');"/>	
+									<input type="text" id="successcode" value="" onclick="LE_Handlers.SelectAll('successcode');"/>
 								</div>
 								<div id="pass_thru_error"></div>
 							</div>
-						
+
 						</fieldset>
 					</form>

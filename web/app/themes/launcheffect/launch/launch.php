ce370ae4faf0836dea9f46ffa86a8aa5d1ccfa04
Andre Garrigo <andre+git@garrigo.net>
2014-12-22 22:16:43 -0500

    Style text that appears after form submission

--- web/app/themes/launcheffect/launch/launch.php
+++ web/app/themes/launcheffect/launch/launch.php
@@ -33,11 +33,59 @@ $placed_content = sprintf('<div id="signup-editor-content">%s</div>', wpautop(ge
 						<div id="presignup-content" class="signup-left">
 							<span class="privacy-policy">Sign Up, Stay in the Know</span>
 						</div>  -->
+
 						<!-- DISPLAY CONFIRMATION TEXT AFTER SIGN UP -->
 						<div id="success-content">
 							<h2>Congrats, You're in the Know!</h2>
 						</div>
-						<?php get_template_part('launch/launch','form'); ?>
+
+						<!-- EMAIL SIGNUP FORM (PRE-SIGNUP) -->
+						<form id="form" action="" class="signup-right">
+							<?php wp_nonce_field('pre_signup','verify_pre_signup'); ?>
+
+							<input type="hidden" id="action" name="action" value="le_submit" />
+							<input type="hidden" id="referredBy" name="referredBy" value="<?php echo @$_GET['ref']; ?>" />
+							<fieldset>
+								<input type="hidden" name="code" id="code" value="<?php codeCheck(); ?>" />
+								<ul id="form-layout">
+									<li class="first">
+
+									    <div id="email-layout">
+									        <span id="emailinput">
+									            <input type="email" id="email" name="email" placeholder="Your Email Address" />
+									        </span>
+
+											<!-- DISPLAY THE SUBMIT BUTTON -->
+											<span id="submit-button-border">
+												<span id="submit-button-spinner"></span>
+												<input type="submit" name="submit" value="Sign Up" id="submit-button" />
+											</span>
+									    </div>
+
+										<!-- HANDLE ERROR MESSAGING -->
+										<div id="error"></div>
+									</li>
+								</ul>
+							</fieldset>
+						</form>
+
+						<!-- EMAIL SIGNUP FORM  (POST-SIGNUP) -->
+						<form id="success" action="">
+							<fieldset>
+
+								<!-- RETURNING USER -->
+								<div id="returninguser">
+									<!-- "Congrats, You're in the Know!" -->
+									<h2>Congrats, You're in the Know!</h2>
+								</div>
+
+								<!-- NEW USER -->
+								<div id="newuser">
+									<div id="pass_thru_error"></div>
+								</div>
+
+							</fieldset>
+						</form>
 					</div>
 
 					<!-- DISPLAY SOCIAL MEDIA ICONS -->
